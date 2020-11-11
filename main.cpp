a82f17c2d584f9d0476009f435492beea0792381
souichirou <souichiroug@gmail.com>
2020-11-11 20:21:53 +0900

    [update]showAST {nkDo, [Type:DO, Literal:do], [....から {kind: nkDo, (Type:DO, Literal:do), [....に変更

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "ka2lib/ka2funcs.h"
 
 int main ( int argc , char *argv[] ) {
-  const std::vector<int> x = { 1 , 2 , -3 } ;
-  ka23::print ( ka23::init ( x ) [ 0 ] ) ;
+  const std::string a = ka23::toString ( 10 ) ;
 }
--- src/ka2show.nim
+++ src/ka2show.nim
@@ -3,7 +3,7 @@ import  ka2parser, ka2rw, ka2node, ka2cpp, ka2shaping
 proc showAST(node: Node, indent: int): string =
   for i in 0..indent-1:
     result.add("  ")
-  result.add("{:" & $node.kind & ", [Type:" & $node.token.Type & ", Literal:" & $node.token.Literal & "], [")
+  result.add("{kind: " & $node.kind & ", (Type:" & $node.token.Type & ", Literal:" & $node.token.Literal & "), [")
   for child in node.child_nodes:
     result.add("\n")
     result.add(showAST(child, indent + 1))
