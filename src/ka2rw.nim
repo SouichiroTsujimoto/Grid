f0b10ac3db2d0e5ef0b7fabb1e295a45f391061e
souichirou <souichiroug@gmail.com>
2021-01-11 22:51:45 +0900

    [update]"funcs_code"を更新

--- src/ka2rw.nim
+++ src/ka2rw.nim
@@ -102,365 +102,77 @@ namespace ka23 {
     return a != b;
   }
 
-  int len(std::vector<int> a) {
-    return (int)a.size();
+  void print(int a) {
+    std::string b = std::to_string(a);
+    std::cout << b;
   }
-  int len(std::initializer_list<int> a) {
-    return (int)a.size();
+  void print(float a) {
+    std::string b = std::to_string(a);
+    std::cout << b;
   }
-  int len(std::vector<float> a) {
-    return (int)a.size();
-  }
-  int len(std::initializer_list<float> a) {
-    return (int)a.size();
-  }
-  int len(std::vector<char> a) {
-    return (int)a.size();
-  }
-  int len(std::initializer_list<char> a) {
-    return (int)a.size();
-  }
-  int len(std::vector<std::string> a) {
-    return (int)a.size();
-  }
-  int len(std::initializer_list<std::string> a) {
-    return (int)a.size();
-  }
-  int len(std::vector<bool> a) {
-    return (int)a.size();
-  }
-  int len(std::initializer_list<bool> a) {
-    return (int)a.size();
-  }
-
-  std::vector<int> join(std::vector<int> a, std::vector<int> b) {
-    std::vector<int> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<int> join(std::initializer_list<int> a, std::vector<int> b) {
-    std::vector<int> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<int> join(std::vector<int> a, std::initializer_list<int> b) {
-    std::vector<int> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<int> join(std::initializer_list<int> a, std::initializer_list<int> b) {
-    std::vector<int> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<float> join(std::vector<float> a, std::vector<float> b) {
-    std::vector<float> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<float> join(std::initializer_list<float> a, std::vector<float> b) {
-    std::vector<float> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<float> join(std::vector<float> a, std::initializer_list<float> b) {
-    std::vector<float> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<float> join(std::initializer_list<float> a, std::initializer_list<float> b) {
-    std::vector<float> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<char> join(std::vector<char> a, std::vector<char> b) {
-    std::vector<char> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<char> join(std::initializer_list<char> a, std::vector<char> b) {
-    std::vector<char> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<char> join(std::vector<char> a, std::initializer_list<char> b) {
-    std::vector<char> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<char> join(std::initializer_list<char> a, std::initializer_list<char> b) {
-    std::vector<char> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<std::string> join(std::vector<std::string> a, std::vector<std::string> b) {
-    std::vector<std::string> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<std::string> join(std::initializer_list<std::string> a, std::vector<std::string> b) {
-    std::vector<std::string> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<std::string> join(std::vector<std::string> a, std::initializer_list<std::string> b) {
-    std::vector<std::string> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<std::string> join(std::initializer_list<std::string> a, std::initializer_list<std::string> b) {
-    std::vector<std::string> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<bool> join(std::vector<bool> a, std::vector<bool> b) {
-    std::vector<bool> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<bool> join(std::initializer_list<bool> a, std::vector<bool> b) {
-    std::vector<bool> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<bool> join(std::vector<bool> a, std::initializer_list<bool> b) {
-    std::vector<bool> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-  std::vector<bool> join(std::initializer_list<bool> a, std::initializer_list<bool> b) {
-    std::vector<bool> c;
-    c.reserve(a.size() + b.size());
-    c.insert(c.end(), a.begin(), a.end());
-    c.insert(c.end(), b.begin(), b.end());
-    return c;
-  }
-
   void print(char a) {
-    std::cout << a;
+    std::string b = std::to_string(a);
+    std::cout << b;
   }
   void print(std::string a) {
     std::cout << a;
   }
+  void print(bool a) {
+    std::string b = std::to_string(a);
+    std::cout << b;
+  }
 
+  void println(int a) {
+    std::string b = std::to_string(a);
+    std::cout << b << "\n";
+  }
+  void println(float a) {
+    std::string b = std::to_string(a);
+    std::cout << b << "\n";
+  }
   void println(char a) {
-    std::cout << a << "\n";
+    std::string b = std::to_string(a);
+    std::cout << b << "\n";
   }
   void println(std::string a) {
     std::cout << a << "\n";
   }
+  void println(bool a) {
+    std::string b = std::to_string(a);
+    std::cout << b << "\n";
+  }
+  
+  auto len = [](auto a) -> int {
+    return (int)a.size();
+  };
 
-  int head(std::vector<int> a) {
-    return a[0];
-  }
-  int head(std::initializer_list<int> a) {
-    std::vector<int> b = a;
-    return b[0];
-  }
-  float head(std::vector<float> a) {
-    return a[0];
-  }
-  float head(std::initializer_list<float> a) {
-    std::vector<float> b = a;
-    return b[0];
-  }
-  char head(std::vector<char> a) {
-    return a[0];
-  }
-  char head(std::initializer_list<char> a) {
-    std::vector<char> b = a;
-    return b[0];
-  }
-  std::string head(std::vector<std::string> a) {
-    return a[0];
-  }
-  std::string head(std::initializer_list<std::string> a) {
-    std::vector<std::string> b = a;
-    return b[0];
-  }
-  bool head(std::vector<bool> a) {
-    return a[0];
-  }
-  bool head(std::initializer_list<bool> a) {
-    std::vector<bool> b = a;
-    return b[0];
-  }
+  auto join = [](auto a, auto b) {
+    decltype(a) c;
+    c.reserve(a.size() + b.size());
+    c.insert(c.end(), a.begin(), a.end());
+    c.insert(c.end(), b.begin(), b.end());
+    return c;
+  };
 
-  std::vector<int> tail(std::vector<int> a) {
-    std::vector<int> b(a.size()-1);
-    copy(a.begin()+1, a.end(), b.begin());
-    return b;
-  }
-  std::vector<int> tail(std::initializer_list<int> a) {
-    std::vector<int> b(a.size()-1);
-    copy(a.begin()+1, a.end(), b.begin());
-    return b;
-  }
-  std::vector<float> tail(std::vector<float> a) {
-    std::vector<float> b(a.size()-1);
-    copy(a.begin()+1, a.end(), b.begin());
-    return b;
-  }
-  std::vector<float> tail(std::initializer_list<float> a) {
-    std::vector<float> b(a.size()-1);
-    copy(a.begin()+1, a.end(), b.begin());
-    return b;
-  }
-  std::vector<char> tail(std::vector<char> a) {
-    std::vector<char> b(a.size()-1);
-    copy(a.begin()+1, a.end(), b.begin());
-    return b;
-  }
-  std::vector<char> tail(std::initializer_list<char> a) {
-    std::vector<char> b(a.size()-1);
-    copy(a.begin()+1, a.end(), b.begin());
-    return b;
-  }
-  std::vector<std::string> tail(std::vector<std::string> a) {
-    std::vector<std::string> b(a.size()-1);
-    copy(a.begin()+1, a.end(), b.begin());
-    return b;
-  }
-  std::vector<std::string> tail(std::initializer_list<std::string> a) {
-    std::vector<std::string> b(a.size()-1);
-    copy(a.begin()+1, a.end(), b.begin());
-    return b;
-  }
-  std::vector<bool> tail(std::vector<bool> a) {
-    std::vector<bool> b(a.size()-1);
-    copy(a.begin()+1, a.end(), b.begin());
-    return b;
-  }
-  std::vector<bool> tail(std::initializer_list<bool> a) {
-    std::vector<bool> b(a.size()-1);
-    copy(a.begin()+1, a.end(), b.begin());
-    return b;
-  }
+  auto head = [](auto a) {
+    return a[0];
+  };
 
-  int last(std::vector<int> a) {
-    return a.back();
-  }
-  int last(std::initializer_list<int> a) {
-    std::vector<int> b = a;
-    return b.back();
-  }
-  float last(std::vector<float> a) {
-    return a.back();
-  }
-  float last(std::initializer_list<float> a) {
-    std::vector<float> b = a;
-    return b.back();
-  }
-  char last(std::vector<char> a) {
-    return a.back();
-  }
-  char last(std::initializer_list<char> a) {
-    std::vector<char> b = a;
-    return b.back();
-  }
-  std::string last(std::vector<std::string> a) {
-    return a.back();
-  }
-  std::string last(std::initializer_list<std::string> a) {
-    std::vector<std::string> b = a;
-    return b.back();
-  }
-  bool last(std::vector<bool> a) {
-    return a.back();
-  }
-  bool last(std::initializer_list<bool> a) {
-    std::vector<bool> b = a;
-    return b.back();
-  }
+  auto tail = [](auto a) {
+    decltype(a) b(a.size()-1);
+    copy(a.begin()+1, a.end(), b.begin());
+    return b;
+  };
 
-  std::vector<int> init(std::vector<int> a) {
-    std::vector<int> b(a.size()-1);
+  auto last = [](auto a) {
+    return a.back();
+  };
+
+  auto init = [](auto a) {
+    decltype(a) b(a.size()-1);
     copy(a.begin(), a.end()-1, b.begin());
     return b;
-  }
-  std::vector<int> init(std::initializer_list<int> a) {
-    std::vector<int> b(a.size()-1);
-    copy(a.begin(), a.end()-1, b.begin());
-    return b;
-  }
-  std::vector<float> init(std::vector<float> a) {
-    std::vector<float> b(a.size()-1);
-    copy(a.begin(), a.end()-1, b.begin());
-    return b;
-  }
-  std::vector<float> init(std::initializer_list<float> a) {
-    std::vector<float> b(a.size()-1);
-    copy(a.begin(), a.end()-1, b.begin());
-    return b;
-  }
-  std::vector<char> init(std::vector<char> a) {
-    std::vector<char> b(a.size()-1);
-    copy(a.begin(), a.end()-1, b.begin());
-    return b;
-  }
-  std::vector<char> init(std::initializer_list<char> a) {
-    std::vector<char> b(a.size()-1);
-    copy(a.begin(), a.end()-1, b.begin());
-    return b;
-  }
-  std::vector<std::string> init(std::vector<std::string> a) {
-    std::vector<std::string> b(a.size()-1);
-    copy(a.begin(), a.end()-1, b.begin());
-    return b;
-  }
-  std::vector<std::string> init(std::initializer_list<std::string> a) {
-    std::vector<std::string> b(a.size()-1);
-    copy(a.begin(), a.end()-1, b.begin());
-    return b;
-  }
-  std::vector<bool> init(std::vector<bool> a) {
-    std::vector<bool> b(a.size()-1);
-    copy(a.begin(), a.end()-1, b.begin());
-    return b;
-  }
-  std::vector<bool> init(std::initializer_list<bool> a) {
-    std::vector<bool> b(a.size()-1);
-    copy(a.begin(), a.end()-1, b.begin());
-    return b;
-  }
+  };
 
   std::string toString(int a) {
     return std::to_string(a);
@@ -483,112 +195,17 @@ namespace ka23 {
     }
   }
 
-  std::vector<int> map(std::vector<int> a, std::function<int(int)> b) {
-    std::vector<int> c = {};
-    for(int d : a) {
-      c.push_back(b(d));
-    }
-    return c;
-  }
-  std::vector<int> map(std::initializer_list<int> a, std::function<int(int)> b) {
-    std::vector<int> c = {};
-    for(int d : a) {
-      c.push_back(b(d));
-    }
-    return c;
-  }
-  std::vector<float> map(std::vector<float> a, std::function<float(float)> b) {
-    std::vector<float> c = {};
-    for(float d : a) {
-      c.push_back(b(d));
-    }
-    return c;
-  }
-  std::vector<float> map(std::initializer_list<float> a, std::function<float(float)> b) {
-    std::vector<float> c = {};
-    for(float d : a) {
-      c.push_back(b(d));
-    }
-    return c;
-  }
-  std::vector<char> map(std::vector<char> a, std::function<char(char)> b) {
-    std::vector<char> c = {};
-    for(char d : a) {
-      c.push_back(b(d));
-    }
-    return c;
-  }
-  std::vector<char> map(std::initializer_list<char> a, std::function<char(char)> b) {
-    std::vector<char> c = {};
-    for(char d : a) {
-      c.push_back(b(d));
-    }
-    return c;
-  }
-  std::vector<std::string> map(std::vector<std::string> a, std::function<std::string(std::string)> b) {
-    std::vector<std::string> c = {};
-    for(std::string d : a) {
-      c.push_back(b(d));
-    }
-    return c;
-  }
-  std::vector<std::string> map(std::initializer_list<std::string> a, std::function<std::string(std::string)> b) {
-    std::vector<std::string> c = {};
-    for(std::string d : a) {
-      c.push_back(b(d));
-    }
-    return c;
-  }
-  std::vector<bool> map(std::vector<bool> a, std::function<bool(bool)> b) {
-    std::vector<bool> c = {};
-    for(bool d : a) {
-      c.push_back(b(d));
-    }
-    return c;
-  }
-  std::vector<bool> map(std::initializer_list<bool> a, std::function<bool(bool)> b) {
-    std::vector<bool> c = {};
-    for(bool d : a) {
-      c.push_back(b(d));
-    }
-    return c;
-  }
+  auto at = [](auto a, auto b) {
+    return a[b];
+  };
 
-  int at(std::vector<int> a, int b) {
-    return a[b];
-  }
-  int at(std::initializer_list<int> a, int b) {
-    std::vector<int> c = a;
-    return c[b];
-  }
-  float at(std::vector<float> a, int b) {
-    return a[b];
-  }
-  float at(std::initializer_list<float> a, int b) {
-    std::vector<float> c = a;
-    return c[b];
-  }
-  char at(std::vector<char> a, int b) {
-    return a[b];
-  }
-  char at(std::initializer_list<char> a, int b) {
-    std::vector<char> c = a;
-    return c[b];
-  }
-  std::string at(std::vector<std::string> a, int b) {
-    return a[b];
-  }
-  std::string at(std::initializer_list<std::string> a, int b) {
-    std::vector<std::string> c = a;
-    return c[b];
-  }
-  bool at(std::vector<bool> a, int b) {
-    return a[b];
-  }
-  bool at(std::initializer_list<bool> a, int b) {
-    std::vector<bool> c = a;
-    return c[b];
-  }
+  auto map = [](auto a, auto b) {
+    decltype(a) c = {};
+    for(auto d : a) {
+      c.push_back(b(d));
+    }
+    return c;
+  };
 
   std::string readln () {
     std::string a = "";
